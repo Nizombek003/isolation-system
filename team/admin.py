b295cbdadbd42a3fadf94e966e292b4ca588c803
Nizombek003 <158492563+Nizombek003@users.noreply.github.com>
2026-03-05 19:14:47 +0500

    fix admin 500 error

--- team/admin.py
+++ team/admin.py
@@ -1,22 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from .models import TeamMember, HealthData, ClinicSettings
 
-
-# ==========================================
-# TEAM MEMBER ADMIN
-# ==========================================
 
 @admin.register(TeamMember)
 class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ("full_name", "position")
     search_fields = ("full_name",)
 
-    def has_view_permission(self, request, obj=None):
-        return request.user.is_superuser or request.user.groups.filter(name='Admin').exists()
-# ==========================================
-# HEALTH DATA ADMIN
-# ==========================================
 
 @admin.register(HealthData)
 class HealthDataAdmin(admin.ModelAdmin):
@@ -25,37 +15,7 @@ class HealthDataAdmin(admin.ModelAdmin):
     search_fields = ("member__full_name",)
     readonly_fields = ("risk_score", "risk_level")
 
-    def has_view_permission(self, request, obj=None):
-        return request.user.is_superuser or request.user.groups.filter(name='Admin').exists() or request.user.groups.filter(name='Doctor').exists()
-
-# ==========================================
-# CLINIC SETTINGS (FAKAT 1 TA BO‘LADI)
-# ==========================================
 
 @admin.register(ClinicSettings)
 class ClinicSettingsAdmin(admin.ModelAdmin):
     list_display = ("name", "phone")
-    search_fields = ("name",)
-
-    def has_add_permission(self, request):
-        if ClinicSettings.objects.exists():
-            return False
-        return request.user.is_superuser
-    def has_module_permission(self, request):
-        # Doctor ko‘rmasin
-        if request.user.groups.filter(name='Doctor').exists():
-            return False
-        return request.user.is_superuser or request.user.groups.filter(name='Admin').exists()
-
-
-# ==========================================
-# GROUP ADMIN (faqat superuser)
-# ==========================================
-
-class GroupAdmin(admin.ModelAdmin):
-    def has_module_permission(self, request):
-        return request.user.is_superuser
-
-
-admin.site.unregister(Group)
-admin.site.register(Group, GroupAdmin)
