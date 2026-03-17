7ac2c63e1ec16f37c0356d05369a28d5479380a2
Nizombek003 <158492563+Nizombek003@users.noreply.github.com>
2026-03-18 00:14:22 +0500

    url xatoligi to'g'rilandi

--- team/urls.py
+++ team/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from .views import (
     CustomLoginView,
@@ -12,6 +13,7 @@ from .views import (
 )
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="dashboard", permanent=False), name="index"),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("dashboard/", dashboard, name="dashboard"),
